5f10311b43ee81b60299a6c20dc1ffc46f01a63a
Balaji <abalaji63030@gmail.com>
2024-09-25 20:17:57 +0530

    production error solved

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,6 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import darkSaasLandingPage from "@/assets/Images/dark-saas-landing-page.png";
+import lightSaasLandingPage from "@/assets/Images/light-saas-landing-page.png";
+import aiStartupLandingPage from "@/assets/Images/ai-startup-landing-page.png";
 // import { li } from "framer-motion/client";
 import { Button } from "./ui/button";
 import Image from "next/image";
